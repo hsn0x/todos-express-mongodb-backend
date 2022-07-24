966033e28b460aae43b1b837fd4c2e7da2b474b2
Hassane Bendaoud <40140987+realHassaneBen@users.noreply.github.com>
2022-07-24 16:31:21 +0100

    Update env.js

--- src/config/env.js
+++ src/config/env.js
@@ -1,5 +1,8 @@
 import dotenv from "dotenv"
 dotenv.config()
 
-const envFile = process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : ".env"
-dotenv.config({ path: envFile })
+const envFile = process.env.NODE_ENV && `.env.${process.env.NODE_ENV}`
+
+if (envFile) {
+    dotenv.config({ path: envFile })
+}
